4bd858f24e313a314983819a69254a3e1005871d
Abu Talib <xoftsisdevloper>
2025-10-09 21:47:37 +0530

    The setting and env changes

--- omeiat/settings.py
+++ omeiat/settings.py
@@ -28,7 +28,6 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework',
     'jazzmin',
-    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
